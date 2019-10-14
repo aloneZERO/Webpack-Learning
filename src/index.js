275634ec4d96462f1e771713a98bd25663c1f313
justzero <alonezero@foxmail.com>
2019-10-14 13:47:09 +0800

    [UPDATE] 完成"管理资源"章节的学习

--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ import _ from 'lodash';
 // 导入的 css 文件源码，
 // 经过 webpack 处理，会动态的在页面生成 style 标签
 import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
 
 function component() {
   var element = document.createElement('div');
   
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  // 将图像添加到我们现有的 div
+  var myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
+
+  console.log(Data);
   
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,45 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
-    rules: [{
-      test: /\.css$/, // 正则匹配文件后缀名
-      use: [ // css 文件都交给这两个 loader 处理
-        'style-loader',
-        'css-loader'
-      ]
-    }]
+    rules: [
+      {
+        test: /\.css$/, // 正则匹配文件后缀名
+        use: [ // css 文件都交给这两个 loader 处理
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: [
+          'csv-loader'
+        ]
+      },
+      {
+        test: /\.xml$/,
+        use: [
+          'xml-loader'
+        ]
+      }
+    ]
   }
 };
