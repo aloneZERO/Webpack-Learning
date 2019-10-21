9b0316ebb703fe2e911dd668ada3f6d5e2bcf2cc
justzero <alonezero@foxmail.com>
2019-10-21 09:50:24 +0800

    [UPDATE] 完成“使用环境变量”章节的学习

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import _ from 'lodash';
-// 导入的 css 文件源码，
-// 经过 webpack 处理，会动态的在页面生成 style 标签
-import './style.css';
-
 function component() {
   var element = document.createElement('div');
+  var child1 = document.createElement('div');
+  var child2 = document.createElement('div');
   
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  // 不要误以为 env 是一个 object
+  // env.NODE_ENV 也不是在浏览器环境中才编译取值的，
+  // 而是再 webpack 打包时就已经取值，然后把 innerHTML 赋值完毕了
+  child1.innerHTML = 'NODE_ENV: ' + env.NODE_ENV;
+  child2.innerHTML = 'Production: ' + env.production;
+
+  element.appendChild(child1);
+  element.appendChild(child2);
   
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,29 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [{
-      test: /\.css$/, // 正则匹配文件后缀名
-      use: [ // css 文件都交给这两个 loader 处理
-        'style-loader',
-        'css-loader'
-      ]
-    }]
+module.exports = env => {
+  // Use env.<YOUR VARIABLE> here:
+  console.log('NODE_ENV: ', env.NODE_ENV) // 'local'
+  console.log('Production: ', env.production) // true
+
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        title: '环境变量'
+      }),
+      new webpack.DefinePlugin({
+        'env.NODE_ENV': JSON.stringify(env.NODE_ENV),
+        'env.production': JSON.stringify(env.production)
+      })
+    ]
   }
 };
