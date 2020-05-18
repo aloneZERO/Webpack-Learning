a9edc2f72d1acad8f8b56f3a0476cbf579f1aaab
justzero <alonezero@foxmail.com>
2020-05-18 23:57:13 +0800

    feat(basic_concept): 热更新、文件指纹和代码压缩

--- 02-basic_concept/README.md
+++ 02-basic_concept/README.md
@@ -42,3 +42,41 @@ thread-loader | 多进程打包 JS 和 CSS
 development | 设置 `process.env.NODE_ENV` 的值为 development。开启 NamedChunksPlugin 和 NamedModulesPlugin
 production | 设置 `process.env.NODE_ENV` 的值为 production。开启 FlagDependencyUsagePlugin、FlagIncludedChunksPlugin、NoduleConcatenationPlugin、NoEmitOnErrorsPlugin、OccurrenceOrderPlugin、SideEffectsFlagPlugin 和 TerserPlugin
 none | 不开启任何优化选项
+
+## 文件监听的原理
+
+轮询判断文件的最后编辑时间是否变化？某个文件发生了变化，并不会立刻告诉监听者，而是先缓存起来，等 aggregateTimeout。
+
+```js
+module.export = {
+    // 默认 false 不开启
+    watch: true,
+    // 只有开启监听模式，watchOptions 才有效
+    watchOptions: {
+        // 不监听的文件或者文件夹，默认为空
+        ignored: /node_modules/,
+        // 监听到变化发生后等 300ms 再去执行，默认 300ms
+        aggregateTimeout: 300,
+        // 轮询，默认每秒 1000 次
+        poll: 1000
+    }
+}
+```
+
+## 热更新的原理
+
+hmr：hot module replacement.
+
+- webpack compile：将 js 编译成 bundle
+- hmr server：将热更新的文件输出给 hmr runtime
+- bundle server：提供文件在浏览器的访问
+- hmr runtime：会被注入到浏览器，更新文件的变化
+- bundle.js：构建输出的文件
+
+## 文件指纹
+
+- Hash：和整个项目的构建相关，只要项目文件有修改，整个项目构建的 hash 值就会更改
+- Chunkhash：和 webpack 打包的 chunk 有关，不同的 entry 会生成不同的 chunkhash 值
+- Contenthash：根据文件内容来定义 hash，文件内容不变，则 contenthash 不变
+
+file-loader 里指定的 hash 和上述不同，它是文件内容的 hash，默认是 md5 生成。
--- 02-basic_concept/src/App.vue
+++ 02-basic_concept/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-      {{message}}
+      <span>{{message}}</span>
+      <img src="./assets/1.png">
+      <img src="./assets/2.jpg">
   </div>
 </template>
 
@@ -15,18 +17,39 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
+@font-face {
+    font-family: 'leo';
+    src: url('./assets/my-font.ttf') format('truetype');
+}
+
+@app-size: 50px;
 #app {
-    width: 150px;
-    height: 50px;
-    margin: 10px;
-    line-height: 50px;
-    text-align: center;
-    background-color: palevioletred;
-    box-shadow: 0 1px 5px rgba(0, 0, 0, .5);
-    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    width: 500px;
+    margin: 10px 0;
+    font-family: 'leo';
     font-size: 20px;
     font-weight: bold;
     color: white;
+
+    > span {
+        display: inline-block;
+        width: 150px;
+        height: @app-size;
+        line-height: @app-size;
+        text-align: center;
+        background-color: palevioletred;
+        border-radius: 5px;
+        box-shadow: 0 1px 5px rgba(0, 0, 0, .5);
+    }
+
+    > img {
+        width: @app-size;
+        height: @app-size;
+        margin-left: 10px;
+        border-radius: 5px;
+    }
 }
 </style>
